95b83e715dd7214658753bcc8679d09e10b9c7e0
alejogonza <alejo.1996.2001@gmail.com>
2019-09-10 23:23:04 +0000

    [fix] remove spaces and comments

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,16 +5,12 @@ stri = str(number)
 last = stri[-1:]
 laststri = str(last)
 intl = int(last)
-
-# less 6 and not 0
 if intl<6 and intl!=0:
     text = "Last digit of "+ stri +" is "+ laststri +" and is less than 6 and not 0"
     print(text)
-# greater of 5
 if intl>5:
     text = "Last digit of "+ stri +" is "+ laststri +" and is greater than 5"
     print(text)
-# if is 0
 if intl==0:
     text = "Last digit of "+ stri +" is "+ laststri +" and is 0"
     print(text)

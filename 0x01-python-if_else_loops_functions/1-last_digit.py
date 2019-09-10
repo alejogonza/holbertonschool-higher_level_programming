51461e4d6f42e38a36214f371c6319710b1370df
alejogonza <alejo.1996.2001@gmail.com>
2019-09-10 23:37:24 +0000

    [fix] pep style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,10 +9,13 @@ elif number >= 0:
     newn = number % 10
 # si es mayor que 5
 if newn > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, newn))
+    print('Last digit of {:d} is {:d} '
+          'and is greater than 5'.format(number, newn))
 # si es igual a 0
 elif newn == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, newn))
+    print('Last digit of {:d} is {:d} '
+          'and is 0'.format(number, newn))
 # si es menor a 6
 elif newn < 6:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, newn))
+    print('Last digit of {:d} is {:d} '
+          'and is less than 6 and not 0'.format(number, newn))

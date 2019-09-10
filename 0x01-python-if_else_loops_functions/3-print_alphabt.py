4f12baff3aeca6f07b77d5995742c72da25a68f8
alejogonza <alejo.1996.2001@gmail.com>
2019-09-10 23:57:44 +0000

    [fix] pep style

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for acci in range(97, 123):
-    if(chr(acci)!='q' and chr(acci)!='e'):
+    if(chr(acci) != 'q' and chr(acci) != 'e'):
         print("{}".format(chr(acci)), end='')

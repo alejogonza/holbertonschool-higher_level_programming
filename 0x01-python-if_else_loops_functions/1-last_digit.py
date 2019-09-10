a31739f6e867d7924c90d631f65d3ae53855250d
alejogonza <alejo.1996.2001@gmail.com>
2019-09-10 23:33:16 +0000

    [fix] fix logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-stri = str(number)
-last = stri[-1:]
-laststri = str(last)
-intl = int(last)
-if intl<6 and intl!=0:
-    text = "Last digit of "+ stri +" is "+ laststri +" and is less than 6 and not 0"
-    print(text)
-if intl>5:
-    text = "Last digit of "+ stri +" is "+ laststri +" and is greater than 5"
-    print(text)
-if intl==0:
-    text = "Last digit of "+ stri +" is "+ laststri +" and is 0"
-    print(text)
+# extraer el numero negativo
+if number < 0:
+    newn = number % -10
+# extraer el numero positivo
+elif number >= 0:
+    newn = number % 10
+# si es mayor que 5
+if newn > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, newn))
+# si es igual a 0
+elif newn == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, newn))
+# si es menor a 6
+elif newn < 6:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, newn))

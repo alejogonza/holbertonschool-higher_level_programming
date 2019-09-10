79173a43930508190a2cd378c3cb02142ff4efd4
alejogonza <alejo.1996.2001@gmail.com>
2019-09-10 04:10:30 +0000

    [feature] add list in c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - entry point
+ * Description: check loop for linked list
+ * @list: * linked list
+ * Return: 0 if no 1 if exists
+ */
+int check_cycle(listint_t *list)
+{
+	const listint_t *listone, *listwo = NULL;
+
+
+	if (list == NULL)
+		return (0);
+
+	listone, listwo = list;
+
+	while (listone && listwo && listwo->next)
+	{
+		listone = listone->next;
+		listwo = listwo->next->next;
+		if (listone == listwo)
+			return (1);
+	}
+	return (0);
+}

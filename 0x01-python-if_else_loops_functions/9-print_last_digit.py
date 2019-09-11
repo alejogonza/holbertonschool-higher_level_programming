e7185692e178d6b5dd8d64bb9b66180caa8de75c
alejogonza <alejo.1996.2001@gmail.com>
2019-09-11 01:25:27 +0000

    [feature] print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    stri = str(number)
+    last = stri[-1:]
+    laststri = str(last)
+    intl = int(last)
+    print('{:d}'.format(intl), end='')
+    return intl

41ee05e5497f9baf536dd02d63cbd5575b013b41
alejogonza <alejo.1996.2001@gmail.com>
2019-09-11 06:18:53 +0000

    [fix] spaces .-.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-
+    import sys
     args = (sys.argv)
     argslen = len(sys.argv)
     rest = argslen - 1
-
     if rest == 0:
         print ("{:d} arguments.".format(rest))
     if rest == 1:

71d593d28f2e6354e89d9a7fdac97190b98b82bd
alejogonza <alejo.1996.2001@gmail.com>
2019-09-11 06:09:19 +0000

    [feature] args counter

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+
+    args = (sys.argv)
+    argslen = len(sys.argv)
+    rest = argslen - 1
+
+    if rest == 0:
+        print ("{:d} arguments.".format(rest))
+    if rest == 1:
+        print ("{:d} argument:".format(rest))
+        print("{:d}: {:s}".format(rest, args[1]))
+    if rest > 1:
+        print ("{:d} arguments:".format(rest))
+        for a in range(1, argslen):
+            print("{:d}: {:s}".format(a, args[a]))

328894dc81e2ea9a2311dd86a4ee06d414208b12
alejogonza <alejo.1996.2001@gmail.com>
2019-09-11 06:33:57 +0000

    [fix] more spaces

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     argslen = len(sys.argv)
     rest = argslen - 1
     if rest == 0:
-        print ("{:d} arguments.".format(rest))
+        print("{:d} arguments.".format(rest))
     if rest == 1:
-        print ("{:d} argument:".format(rest))
+        print("{:d} argument:".format(rest))
         print("{:d}: {:s}".format(rest, args[1]))
     if rest > 1:
-        print ("{:d} arguments:".format(rest))
+        print("{:d} arguments:".format(rest))
         for a in range(1, argslen):
             print("{:d}: {:s}".format(a, args[a]))

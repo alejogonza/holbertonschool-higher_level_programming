ecc4e9b2c7bcfea0c05163bd9f53613c2e6b36f7
alejogonza <alejo.1996.2001@gmail.com>
2019-09-17 07:05:56 +0000

    [fix] pep

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     div = []
-    if my_list != "":
+    n = None
+    if my_list != n:
         for reco in my_list:
             if (reco % 2) == 0:
                 div.append(True)

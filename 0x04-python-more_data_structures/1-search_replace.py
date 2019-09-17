2f68b624f1e6e663379b1eaf9334f8eaa0b16b6e
alejogonza <alejo.1996.2001@gmail.com>
2019-09-17 18:02:53 +0000

    [fix] none at the list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list == []:
-        return (None)
+        return (my_list)
     if search > len(my_list):
-        return (None)
+        return (my_list)
     cplist = my_list.copy()
     s = search - 1
     cplist[s] = replace

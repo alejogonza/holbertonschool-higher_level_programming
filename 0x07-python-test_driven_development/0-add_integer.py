44c1a570016b337fbc303a2916c47e01a48df560
alejogonza <alejo.1996.2001@gmail.com>
2019-10-02 07:14:30 +0000

    [fix] add comments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
+"""
+
+The module function: add_integer().
+
+make test: python3 -m doctest -v ./tests/0-add_integer.txt
+
+"""
+
+
 def add_integer(a, b=98):
+    """
+    calculate the sum of two ints
+
+    Args:
+    a: int, if is float cast to integer
+    b: int, if is float cast to integer
+
+    Return:
+    the sum or error
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):

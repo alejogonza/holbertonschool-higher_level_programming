e92e47bf64a92ef38913af846ffbedbc8dbb1b1c
alejogonza <alejo.1996.2001@gmail.com>
2019-10-05 07:56:20 +0000

    [fix] err test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,9 +7,15 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def testint(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1]), 1)
+        self.assertAlmostEqual(max_integer(), None)
     def testnotint(self):
-        with self.assertRaises(TypeError):
-            max_integer(['a', 2, 3, 4])
+        self.assertRaises(TypeError, max_integer, 123)
+        self.assertRaises(TypeError, max_integer, True)
 
 if __name__ == '__main__':
     unittest.main(exit=False)

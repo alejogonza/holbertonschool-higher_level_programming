d7ecf4ccd47e4817ab2788c601c3de966c0f3d20
alejogonza <alejo.1996.2001@gmail.com>
2019-10-17 01:48:06 +0000

    [fix] 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
 9-add_item
-
 """
-
 import sys
 import os.path
-
-load_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
 save_json_file = __import__('7-save_to_json_file').save_to_json_file
 
+load_json_file = __import__('8-load_from_json_file').load_from_json_file
+
 formater = []
+
 if os.path.exists("add_item.json"):
     formater = load_json_file("add_item.json")
 

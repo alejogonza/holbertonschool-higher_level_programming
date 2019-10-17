f8c34275e1745194baf775a9c109379c59861db2
alejogonza <alejo.1996.2001@gmail.com>
2019-10-17 01:50:36 +0000

    [fix] 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,16 +6,14 @@ import sys
 import os.path
 
 
-save_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_file = __import__('7-save_to_json_file').save_to_json_file
+load_file = __import__('8-load_from_json_file').load_from_json_file
 
-load_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-formater = []
-
+my_list = []
 if os.path.exists("add_item.json"):
-    formater = load_json_file("add_item.json")
+    my_list = load_file("add_item.json")
 
 for arg in sys.argv[1:]:
-    formater.append(arg)
+    my_list.append(arg)
 
-save_json_file(formater, "add_item.json")
+save_file(my_list, "add_item.json")

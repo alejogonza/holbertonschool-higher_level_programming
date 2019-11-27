c616e417db88437e8dc9723f3b473cb8755ae24e
alejogonza <alejo.1996.2001@gmail.com>
2019-11-26 19:15:52 -0500

    [fix] order

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT name AS genre, COUNT(genre_id) AS number_shows FROM tv_show_genres
 JOIN tv_genres
 ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY number_shows DESC; 
+ORDER BY number_shows DESC, tv_genres.id ASC;; 

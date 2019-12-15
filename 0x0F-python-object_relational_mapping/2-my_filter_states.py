1d60168cc1c789896dfff92e91a61fe78fcadf52
alejogonza <alejo.1996.2001@gmail.com>
2019-12-15 05:30:10 +0000

    [fix] rules

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,12 +13,12 @@ if __name__ == "__main__":
                 port=3306,
                 user=argv[1],
                 passwd=argv[2],
-                charset="utf8",
                 db=argv[3])
         cursor = DataBase.cursor()
-        cursor.execute("SELECT * FROM states \
-        WHERE CONVERT(`name` USING Latin1) \
-        COLLATE Latin1_General_CS = '{}';".format(argv[4]))
-        states = cur.fetchall()
-        for state in states:
-                print(state)
+        cursor.execute("SELECT * FROM states ORDER BY id ASC".format())
+        query_rows = cursor.fetchall()
+        for row in query_rows:
+                if row[1] == argv[4]:
+                        print(row)
+                        cursor.close()
+                        conect.close()

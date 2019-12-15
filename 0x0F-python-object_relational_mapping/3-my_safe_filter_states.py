20a1313e2b8181be72d254af9dba57dca69608b7
alejogonza <alejo.1996.2001@gmail.com>
2019-12-15 05:38:04 +0000

    [fix] rules

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,23 +7,17 @@ if __name__ == "__main__":
         import MySQLdb
         from sys import argv
 
-        if len(argv) != 5:
-                exit(0)
-
-        try:
-                DataBase = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
-                cursor = DataBase.cursor()
-                cursor.execute("SELECT * FROM states \
-                WHERE name LIKE %s ORDER BY id ASC", (argv[4],))
-                res = cursor.fetchall()
-                for row in res:
-                        print(row)
-                        cur.close()
-                        db.close()
-        except:
-                pass
+        DataBase = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3])
+        cursor = DataBase.cursor()
+        cursor.execute("SELECT * FROM states \
+        WHERE name LIKE %s ORDER BY id ASC", (argv[4],))
+        res = cursor.fetchall()
+        for row in res:
+                print(row)
+                cursor.close()
+                DataBase.close()

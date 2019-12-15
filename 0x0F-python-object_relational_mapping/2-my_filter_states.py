cb817eb6141112243796c451acd271539d68fc02
alejogonza <alejo.1996.2001@gmail.com>
2019-12-15 05:31:39 +0000

    [fix] rules

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """
-1-filter_states.py
+2-my_filter_states.py
 """
-
 if __name__ == "__main__":
 
-        import MySQLdb
-        from sys import argv
+    import MySQLdb
+    from sys import argv
 
-        DataBase = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=argv[1],
-                passwd=argv[2],
-                db=argv[3])
-        cursor = DataBase.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC".format())
-        query_rows = cursor.fetchall()
-        for row in query_rows:
-                if row[1] == argv[4]:
-                        print(row)
-                        cursor.close()
-                        conect.close()
+    conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = conect.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC".format())
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    conect.close()
